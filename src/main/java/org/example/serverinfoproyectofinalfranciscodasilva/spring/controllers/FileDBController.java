5c86ae362066b5d08e727f769be394e805572881
franciscodsa <franciscojesusdasilvaarana@gmail.com>
2024-05-16 23:10:19 +0200

    funciona todo, tecnicamente, exceptuando el chat

--- src/main/java/org/example/serverinfoproyectofinalfranciscodasilva/spring/controllers/FileDBController.java
+++ src/main/java/org/example/serverinfoproyectofinalfranciscodasilva/spring/controllers/FileDBController.java
@@ -6,6 +6,7 @@ import org.example.serverinfoproyectofinalfranciscodasilva.domain.model.AppMessa
 import org.example.serverinfoproyectofinalfranciscodasilva.domain.services.FileDBServices;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -29,13 +30,32 @@ public class FileDBController {
         }
     }
 
-    @GetMapping("/download/{fileId}")
+  /*  @GetMapping("/download/{fileId}")
     public ResponseEntity<byte[]> getFile(@PathVariable Long fileId){
         FilesDB filesDB = fileDBServices.getFile(fileId);
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filesDB.getFileName() + "\"")
                 .body(filesDB.getData());
+    }*/
+    @GetMapping("/download/{fileId}")
+    public ResponseEntity<byte[]> getFile(@PathVariable Long fileId) {
+        FilesDB filesDB = fileDBServices.getFile(fileId);
+
+        if (filesDB != null) {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.parseMediaType(filesDB.getFileType())); // Establece el tipo MIME correcto
+            headers.setContentLength(filesDB.getData().length); // Establece la longitud del contenido
+
+
+            // Agregar el encabezado Content-Disposition con el nombre del archivo
+            String fileName = filesDB.getFileName(); // Reemplaza esto con el nombre del archivo real
+            headers.setContentDispositionFormData("attachment", fileName);
+            // Retorna la respuesta sin establecer el encabezado Content-Disposition
+            return new ResponseEntity<>(filesDB.getData(), headers, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
