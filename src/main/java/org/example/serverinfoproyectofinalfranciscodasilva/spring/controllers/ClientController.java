5be34c52749757a3df58f0768d91ef0f467a4bdc
franciscodsa <franciscojesusdasilvaarana@gmail.com>
2024-05-24 22:15:31 +0200

    Ya se puede quitar el contador a un cliente y hacer que no tenga contador asignado

--- src/main/java/org/example/serverinfoproyectofinalfranciscodasilva/spring/controllers/ClientController.java
+++ src/main/java/org/example/serverinfoproyectofinalfranciscodasilva/spring/controllers/ClientController.java
@@ -23,6 +23,12 @@ public class ClientController {
         return ResponseEntity.status(HttpStatus.OK).body(new AppMessage("Cliente agregado"));
     }
 
+    @PostMapping("/update")
+    public ResponseEntity<AppMessage> updateClient(@RequestBody Client client) {
+        clientServices.updateClient(client);
+        return ResponseEntity.status(HttpStatus.OK).body(new AppMessage("Actualizado"));
+    }
+
     @GetMapping
     public List<Client> getAllClients() {
         return clientServices.getAll();
@@ -37,23 +43,18 @@ public class ClientController {
     public List<Client> getClientsByAccountantEmail(String accountantEmail){
         return clientServices.getClientsByAccountantEmail(accountantEmail);
     }
-
     //todo: esto no va a hacer falta porque el detealle lo estoy cogiendo directo de la lista de clientes
+
     @GetMapping("/{clientEmail}")
     public Client getClientByEmail(@PathVariable String clientEmail) {
         return clientServices.getClientByEmail(clientEmail);
     }
 
-
     //todo: esto no va a hacer falta porque se eliminaran con el endpoint de users
+
     @DeleteMapping("/delete/{clientEmail}")
     public ResponseEntity<AppMessage> deleteClient(@PathVariable String clientEmail) {
         clientServices.deleteByEmail(clientEmail);
         return ResponseEntity.status(HttpStatus.OK).body(new AppMessage("Eliminado"));
-    }
-
-    @PostMapping("/update")
-    public ResponseEntity<Client> updateClient(@RequestBody Client client) {
-        return ResponseEntity.ok(clientServices.updateClient(client));
     }
 }
